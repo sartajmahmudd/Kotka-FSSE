cef45321247d13b9f127218d657a4b063b89c8db
sartajmahmud <oishik1999@gmail.com>
2025-05-10 22:26:51 +0600

    Added steps/commands for git initialization and Git AccessToken in Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,3 +3,23 @@ public class Main {
         System.out.println("Hello World");
     }
 }
+
+
+/*
+GIT Initialization commands
+
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M master
+git remote add origin https://github.com/sartajmahmudd/Kotka-FSSE
+git push -u origin master
+
+
+Get Git AccessToken
+
+Log into Github.com
+go to this URL -> https://github.com/settings/tokens
+you can generate new token
+
+ */
